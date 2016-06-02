6be16c42f3c08bde6991320b1f19f6a464c9fdab
Josh David Miller <josh@joshdmiller.com>
2016-06-02 11:32:09 -0700

    feat: add geo coords to geolocation demo

--- js/routes/Geolocation.js
+++ js/routes/Geolocation.js
@@ -12,10 +12,30 @@ import stylesheet from '../stylesheet';
 
 export default ( React : Object ) => rs( React ).compose({
   init () {
+    this.state = {
+      position: { coords: {} },
+    };
+  },
+
+  componentDidMount () {
+    navigator.geolocation.getCurrentPosition(
+      position => this.setState({ position }),
+      err => console.warn( err.message ),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+
+    this.geoWatchId = navigator.geolocation.watchPosition(
+      position => this.setState({ position })
+    );
+  },
+
+  componentWillUnmount () {
+    navigator.geolocation.clearWatch( this.geoWatchId );
   },
 
   render () {
-    console.log("render")
+    const { position: { coords } } = this.state;
+
     return (
       <View style={styles.container}>
         <MapView
@@ -23,6 +43,15 @@ export default ( React : Object ) => rs( React ).compose({
           showsUserLocation={true}
           followUserLocation={true}
         />
+
+        <View style={styles.coords}>
+          <Text style={styles.label}>
+            Lat, Long:
+          </Text>
+          <Text style={styles.value}>
+            {`( ${coords.latitude}, ${coords.longitude} )`}
+          </Text>
+        </View>
       </View>
     );
   },
@@ -36,9 +65,26 @@ const styles = stylesheet({
 
   map: {
     flex: 1,
-    margin: 20,
+    margin: 10,
     borderColor: '#aaaaaa',
     borderWidth: 1,
+  },
+
+
+  coords: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+
+    flexDirection: 'row',
+  },
+
+  label: {
+    fontWeight: 'bold',
+  },
+
+  value: {
+    fontStyle: 'italic',
   },
 });
 
